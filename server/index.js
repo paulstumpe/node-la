40e9d40eb18ad26290fb378c67fcacf008e60a46
Ryan Moragas <ryanmoragas@gmail.com>
2019-12-26 15:56:53 -0600

    rendering weird stuff

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-//TODO:create connection using Feathers
+//TODO:create connection using Feathersn
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+const CLIENT_PATH = path.join(__dirname, '../client/dist/');
+
+app.use(express.static(CLIENT_PATH));
+app.listen(PORT, () => {
+  console.log(`Listening on :${PORT}`);
+});
