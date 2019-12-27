f139f889c03d67412206156f4478da6579e8b1fd
Matthew Hritz <jillianntish@gmail.com>
2019-12-27 12:16:47 -0600

    add mariaDB to package dependencies

--- schema.sql
+++ schema.sql
@@ -1,1 +1,39 @@
- // TODO:construct schema;
+CREATE DATABASE IF NOT EXISTS nodela;
+
+USE nodela;
+
+CREATE TABLE IF NOT EXISTS users(
+id INTEGER NOT NULL AUT
+username V
+password
+);
+
+CREATE TABLE IF NOT EXISTS posts(
+  id 
+  username 
+  body 
+  votes 
+  time 
+  hood_id fk hoods id
+  post_id  fk users id
+  type_id
+)
+
+CREATE TABLE IF NOT EXISTS post_type(
+  id 
+  help_gen
+)
+
+CREATE TABLE IF NOT EXISTS comments(
+  id 
+  id_user FK
+  id_post FK
+  body
+  time
+)
+
+CREATE TABLE IF NOT EXISTS hoods(
+  id fk posts id_types
+  up_dwn
+  hood_name
+)
