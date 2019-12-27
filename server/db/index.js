3fc255bb976f998445795b474ff68d837cb89a15
Matthew Hritz <jillianntish@gmail.com>
2019-12-27 12:54:39 -0600

    add db configurations

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,9 @@
-//TODO: create connection  to mariaDB
+const mariadb = require('mariadb');
+const mariaConfig = require('./config')
+
+
+mariadb.createConnection(mariaConfig)
+  .then(conn => console.log('connected to the db! with the id' + conn.threadId))
+  .catch(err => console.log(err))
+
+
