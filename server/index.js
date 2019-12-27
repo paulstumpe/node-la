a2b327c9f0e0241188abe9142e835f4884c67083
Matthew Hritz <jillianntish@gmail.com>
2019-12-27 17:59:57 -0600

    create fethersjs server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,63 @@
-//TODO:create connection using Feathers
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
+// Creates an ExpressJS compatible Feathers application
+const app = express(feathers());
+
+// Parse HTTP JSON bodies
+app.use(express.json());
+// Parse URL-encoded params
+app.use(express.urlencoded({ extended: true }));
+// Host static files from the current folder
+app.use(express.static(__dirname));
+// Add REST API support
+app.configure(express.rest());
+// Configure Socket.io real-time APIs
+app.configure(socketio());
+// Register an in-memory messages service
+app.use('/posts', new PostService());
+// Register a nicer error handler than the default Express one
+app.use(express.errorHandler());
+
+// Add any new real-time connection to the `everybody` channel
+app.on('connection', connection =>
+  app.channel('everybody').join(connection)
+);
+// Publish all events to the `everybody` channel
+app.publish(data => app.channel('everybody'));
+
+
+// A messages service that allows to create new
+// and return all existing messages
+class PostsService {
+  constructor() {
+    this.posts = [];
+  }
+  async find () {
+    // Just return all our posts
+    return this.posts;
+  }
+  async create (data) {
+    // The new message is the data merged with a unique identifier
+    // using the messages length since it changes whenever we add one
+    const post = {
+      id: this.posts.length,
+      text: data.text
+    }
+    // Add new message to the list
+    this.posts.push(post);
+    return post;
+  }
+}
+
+
+// Start the server
+app.listen(3030).on('listening', () =>
+  console.log('Feathers server listening on localhost:3030')
+);
+
+// For good measure let's create a message
+// So our API doesn't look so empty
+app.service('messages').create({
+  text: 'Hello world from the server'
+});
