228659d8ffcab04c5a730162fdc5fbb5b941ce49
Matthew Hritz <jillianntish@gmail.com>
2019-12-27 18:02:03 -0600

    create sqlize instance

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,21 @@
 const mariadb = require('mariadb');
 const mariaConfig = require('./config')
+const Sequelize = require('sequelize');
 
 
 mariadb.createConnection(mariaConfig)
   .then(conn => console.log('connected to the db! with the id' + conn.threadId))
   .catch(err => console.log(err))
 
+const sqlize = new Sequelize('nodela', 'root', '', {
+  dialect: "mariadb", // or 'sqlite', 'postgres', 'mariadb'
+  port:    3306, // or 5432 (for postgres)
+});
 
+sqlize
+.authenticate()
+.then(function(err) {
+console.log('Connection has been established successfully.');
+}, function (err) { 
+console.log('Unable to connect to the database:', err);
+});
