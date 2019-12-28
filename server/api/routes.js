c818b2812ee00be9cb4b2ecb40a96676c0c23e79
Matthew Hritz <jillianntish@gmail.com>
2019-12-28 10:25:00 -0600

    add BoardPost handler route

---
+++ server/api/routes.js
@@ -0,0 +1,36 @@
+
+//const feathers = require('@feathersjs/feathers');
+//const express = require('@feathersjs/express');
+//const socketio = require('@feathersjs/socketio');
+//const app = express(feathers());
+
+
+// A messages service that allows to create new
+// and return all existing messages
+/*
+This class follows the feathers js doc to a t utilizing
+express as a way to run a RESTFUL api server
+*/
+
+class MessageBoardService {
+  constructor() {
+    this.board = [];
+  }
+  async find() {
+    // Just return all our messages
+    return this.board;
+  }
+  async create (post) {
+    // The new message is the data merged with a unique identifier
+    // using the messages length since it changes whenever we add one
+    const messageBoardPost = {
+      id: this.board.length,
+      text: post.text
+    }
+    // Add new message to the list
+    this.board.push(messageBoardPost);
+    return messageBoardPost;
+  }
+}
+
+module.exports.MessageBoardService = MessageBoardService;
