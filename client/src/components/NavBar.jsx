5146087732bac7829f88df9b95538379b741be0a
Ryan Moragas <ryanmoragas@gmail.com>
2019-12-27 21:26:51 -0600

    completed login compnent layout

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-//style imports for all material ui css
-import { makeStyles } from '@material-ui/core/styles';
-//all component imports needed for navbar
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
-//all material icon imports
-import MenuIcon from '@material-ui/icons/Menu';
-//all component imports needed for login dialog box
-import { TextField, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
+//style imports for all material ui 
+import MenuList from './MenuList.jsx';
+import Login from './Login.jsx'
+import { makeStyles, AppBar, Toolbar, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,43 +18,18 @@ const useStyles = makeStyles(theme => ({
 
 const NavBar = () => {
   const classes = useStyles();
-
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           {/* menu button */}
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
+          <MenuList />
           {/* app title */}
           <Typography variant="h5" className={classes.title}> Node.LA </Typography>
           {/* Login button */}
-          <Button variant="contained" color="secondary" onClick={handleOpen}> Login </Button>
-          {/* dialog box for loging in */}
-          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title"> Login </DialogTitle>
-            {/* text fields in dialog box */}
-            <DialogContent>
-              <TextField id="username" label="Username" type="username" fullWidth />
-              <TextField id="password" label="Password" type="password" fullWidth />
-            </DialogContent>
-            {/* buttons in dialog box */}
-            <DialogActions>
-              <Button onClick={handleClose} color="primary">Cancel</Button>
-              <Button onClick={handleClose} color="primary">Login</Button>
-            </DialogActions>
-          </Dialog>
+          <div variant="contained" color="secondary"> 
+            <Login />
+          </div>
         </Toolbar>
       </AppBar>
     </div>
