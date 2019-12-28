a5266ed2ae695e534dc20b89e773d16c37245d8b
Matthew Hritz <jillianntish@gmail.com>
2019-12-28 14:04:50 -0600

    save usernames to the database

--- server/index.js
+++ server/index.js
@@ -2,17 +2,25 @@ const path = require('path');
 const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
-const app = express(feathers());
-const { MessageBoardService }  = require('./api/routes')
-
-
+const Sequelize = require('sequelize')
+const service  = require('feathers-sequelize')
+const { MessageBoardService }  = require('./api/helpers')
+const CLIENT_PATH = path.join(__dirname, '../client/dist');
 const PORT = process.env.PORT || 3000;
+const mariaConfig = require('./db/config')
+
+const sequelize = new Sequelize('nodela', 'root', '', mariaConfig);
+
+sequelize.authenticate()
+  .then(() => console.log('Connection to the database has been established successfully.'))
+  .catch(err => console.log('Database Connection Error', err));
+
+const app = express(feathers());
 // Parse HTTP JSON bodies
 app.use(express.json());
 // Parse URL-encoded params
 app.use(express.urlencoded({ extended: true }));
 // Host static files from the current folder
-const CLIENT_PATH = path.join(__dirname, '../client/dist');
 app.use(express.static(CLIENT_PATH));
 // Add REST API support
 app.configure(express.rest());
@@ -25,17 +33,26 @@ app.use('/posts', new MessageBoardService());
 
 // Log every time a new message has been created
 app.service('posts').on('created', post => {
-  
   console.log('A new post has been created on the forum', post);
 });
+
+const User = sequelize.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+}
+})
+
+User.sync({force: true})
+.then(() => {
+  return User.create({
+    username: 'testUser'
+  })
+})
 
 app.service('posts').create({
   text: 'Hello world from the server'
 });
-
-
-
-
 
 app.listen(PORT).on('listening', () =>
   console.log(`Feathers server is listening on ${PORT}! 🚀`)
