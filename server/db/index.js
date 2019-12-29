68b2e033113f1d912e77956eb5672ccab1577e7e
Matthew Hritz <jillianntish@gmail.com>
2019-12-29 16:38:52 -0600

    transfer db construct to db folder from server ind

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const util = require('util')
 const mariaConfig = require('./config')
 const Sequelize = require('sequelize');
 
-//const sequelize = new Sequelize('nodela', 'root', '', mariaConfig);
+const sequelize = new Sequelize('nodela', 'root', '', mariaConfig);
 
 //const query = util.promisify(sequelize.query).bind(sequelize)
 //must use this specific syntax to connect with sequelize
@@ -10,6 +10,153 @@ const Sequelize = require('sequelize');
 // const getPosts = () => {
 //   return query('SELECT * FROM users')
 // }
+
+const User = sequelize.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  main_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  }
+})
+
+User.sync({ force: true })
+  .then(() => {
+    return User.create({
+      username: 'testUser'
+    })
+  })
+
+
+  const Hood = sequelize.define('hood', {
+    up_down: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    hood_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  })
+  //on post request to board
+    Hood.sync({ force: true })
+    .then(() => {
+      return Hood.create({
+        up_down: 'downtown',
+        hood_name: 'treme'
+      })
+    })
+
+
+const Post_Type = sequelize.define('post_type', {
+  help_gen: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+})
+
+Post_Type.sync({ force: true })
+  .then(() => {
+    return Post_Type.create({
+      help_gen: 'general'
+    })
+  })
+
+const Post = sequelize.define('post', {
+  post_hood_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: Hood,
+      key: 'id'
+    }
+  },
+  post_type_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true
+  },
+  post_body: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  post_votes: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+
+Post.sync({ force: true })
+  .then(() => {
+    return Post.create({
+      post_hood_id: 1,
+      post_type_id: 1,
+      post_body: 'this is a test post',
+      post_votes: 1
+    })
+  })
+
+
+const Comment = sequelize.define('comment', {
+  comment_user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  comment_post_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: Post,
+      key: 'id'
+    }
+  },
+  comment_body: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  comment_votes: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+
+Comment.sync({ force: true })
+  .then(() => {
+    return Comment.create({
+      comment_user_id: 1,
+      comment_post_id: 1,
+      comment_body: 'this is a test comment',
+      comment_votes: 1
+    })
+  })
+
+
+
+  Hood.hasOne(Post)
+  User.hasOne(Hood)
+  User.hasOne(Post_Type)
+  User.hasMany(Post, {
+  foreignKey: 'main_id',
+  sourceKey: 'main_id',
+  constraints: false
+  }),
+  User.hasMany(Comment)
+  Hood.belongsTo(User)
+  Post.hasOne(Post_Type)
+  Post.hasOne(Hood)
+  Post.hasOne(User)
+  Post.hasMany(Comment)
+  Comment.belongsTo(User)
+  Post_Type.hasOne(Post)
+  Post_Type.belongsTo(Post)
+
 
 
   //get all the posts or comments from the db
@@ -25,6 +172,6 @@ const Sequelize = require('sequelize');
   //sync all models at once to the db instead of individually
   //sqlize.sync()
 
-  module.exports = {
-    getPosts,
-  }
+  // module.exports = {
+  //   getPosts,
+  // }
--- server/schema.sql
+++ server/schema.sql
@@ -1,54 +1,54 @@
---For Best Results, define schema in the CLI an in this particular order so that no reference
---errors are thrown. Foreign Key structure is a mariadb requirement
+-- --For Best Results, define schema in the CLI an in this particular order so that no reference
+-- --errors are thrown. Foreign Key structure is a mariadb requirement
 
 
-DROP DATABASE nodela;
+-- DROP DATABASE nodela;
 
-CREATE DATABASE nodela;
+-- CREATE DATABASE nodela;
 
-USE nodela;
+-- USE nodela;
 
-CREATE TABLE user (
-id INT NOT NULL AUTO_INCREMENT,
-username VARCHAR(20) NOT NULL UNIQUE,
-PRIMARY KEY (id)
-)
+-- CREATE TABLE user (
+-- id INT NOT NULL AUTO_INCREMENT,
+-- username VARCHAR(20) NOT NULL UNIQUE,
+-- PRIMARY KEY (id)
+-- )
 
-CREATE TABLE hood(
-  id INT NOT NULL AUTO_INCREMENT,
-  up_dwn VARCHAR(10) NOT NULL,
-  hood_name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
-)
+-- CREATE TABLE hood(
+--   id INT NOT NULL AUTO_INCREMENT,
+--   up_dwn VARCHAR(10) NOT NULL,
+--   hood_name VARCHAR(50) NOT NULL,
+--   PRIMARY KEY (id)
+-- )
 
-CREATE TABLE post_type (
-  id INT NOT NULL AUTO_INCREMENT,
-  help_gen VARCHAR(20) NOT NULL,
-  PRIMARY KEY(id)
-)
+-- CREATE TABLE post_type (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   help_gen VARCHAR(20) NOT NULL,
+--   PRIMARY KEY(id)
+-- )
 
-CREATE TABLE post (
-  id INT NOT NULL AUTO_INCREMENT,
-  post_id INT NOT NULL,
-  CONSTRAINT `post_id_ref` FOREIGN KEY (post_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE RESTRICT,
-  post_hood_id INT NOT NULL,
-  CONSTRAINT `post_hood_ref` FOREIGN KEY (post_hood_id) REFERENCES hood (id) ON DELETE CASCADE ON UPDATE RESTRICT,
-  post_type_id INT NOT NULL,
-  CONSTRAINT `post_type_id_ref` FOREIGN KEY (post_type_id) REFERENCES post_type (id) ON DELETE CASCADE ON UPDATE RESTRICT,
-  post_body VARCHAR(500) NOT NULL,
-  post_votes INT NOT NULL,
-  PRIMARY KEY (id)
-)
+-- CREATE TABLE post (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   post_id INT NOT NULL,
+--   CONSTRAINT `post_id_ref` FOREIGN KEY (post_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE RESTRICT,
+--   post_hood_id INT NOT NULL,
+--   CONSTRAINT `post_hood_ref` FOREIGN KEY (post_hood_id) REFERENCES hood (id) ON DELETE CASCADE ON UPDATE RESTRICT,
+--   post_type_id INT NOT NULL,
+--   CONSTRAINT `post_type_id_ref` FOREIGN KEY (post_type_id) REFERENCES post_type (id) ON DELETE CASCADE ON UPDATE RESTRICT,
+--   post_body VARCHAR(500) NOT NULL,
+--   post_votes INT NOT NULL,
+--   PRIMARY KEY (id)
+-- )
 
-CREATE TABLE comment(
-  id INT NOT NULL AUTO_INCREMENT,
-  user_comment_id INT NOT NULL,
-  CONSTRAINT `user_comment_id_reference` FOREIGN KEY (user_comment_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE RESTRICT,
-  post_comment_id INT NOT NULL,
-  CONSTRAINT `post_comment_id_reference` FOREIGN KEY (post_comment_id) REFERENCES post (id) ON DELETE CASCADE ON UPDATE RESTRICT,
-  comment_body VARCHAR(500) NOT NULL,
-  comment_votes INT NOT NULL,
-  PRIMARY KEY (id)
-)
+-- CREATE TABLE comment(
+--   id INT NOT NULL AUTO_INCREMENT,
+--   user_comment_id INT NOT NULL,
+--   CONSTRAINT `user_comment_id_reference` FOREIGN KEY (user_comment_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE RESTRICT,
+--   post_comment_id INT NOT NULL,
+--   CONSTRAINT `post_comment_id_reference` FOREIGN KEY (post_comment_id) REFERENCES post (id) ON DELETE CASCADE ON UPDATE RESTRICT,
+--   comment_body VARCHAR(500) NOT NULL,
+--   comment_votes INT NOT NULL,
+--   PRIMARY KEY (id)
+-- )
 
 
