1c6958823ab82df8a850bfda3742e588722d874c
Ryan Moragas <ryanmoragas@gmail.com>
2020-01-01 14:15:08 -0600

    updated back end files

--- server/db/Controllers.js
+++ server/db/Controllers.js
@@ -1,97 +1,205 @@
-const { User, Post, PostTypes, Hood, Comment } = require('../db/index');
+const { User, Post, PostTypes, Hood, Comment } = require('./index');
 const parser = require('body-parser');
 const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
-
 const app = express(feathers());
-
-
-
+// ! USER CRUD
 //create & save a user to the db
-
-//get all the posts or comments from the db based on user id
-
-//save the posts & comments to the db
-
-//delete a post or comment from the db
-
-//select a single post or comment from the db
-
-//update a post or comment in the db
-
-
-//create a user
+// !CREATE USER
 const createUser = function (req, res, next) {
   const username = req.body.username; // Grab username from req body
   const id = req.body.id; // Grab password from req body
   User.create({
-    username: username, 
-    id: id 
-  }).then((data) => {
-    res.status(201).json({ // Send 201 status upon success.
-      status: 'success',
-      data: data,
-      message: `POSTED ${username} TO DATABASE`
+    username: username,
+    id: id
+  })
+    .then((data) => {
+      res.status(201).json({ // Send 201 status upon success.
+        status: 'success',
+        data: data,
+        message: `POSTED ${username} TO DATABASE`
+      });
+    })
+    .catch(err => {
+      res.sendStatus(400)
+      console.log('There was an error adding the user to the db', err);
+      return next();
     });
-  }).catch(err =>  {// Error handling for inner callback create
-    console.log('There was an error adding the user to the db', err);
-    return next();
-  });
-}
-
-const getUsers = function(req, res, next){
-User.findAll({})
-.then((response) => {
-  res.status(200)
-  res.send(JSON.stringify({
-    status: 'success',
-    data: response.data,
-    message: 'Here are all the users!'
-  }))
-  return next();
-})
-.catch(err => {
-  console.log(err)
-  return next();
-})
-}
-
-const getSinglePost = function(req, res) {
-  const id = req.params.goal_id;
-  db.Post.find({
+};
+//user login function
+// ! READ
+const getSingleUser = function (req, res, next) {
+  const id = req.params.id;
+  User.findOrCreate({
     where: {
       id: id
     }
   })
-  .then((singlePost) => {
-    res.status(200).json({
-      data: singlePost
+    .then((data) => { // Find the user with the given auth0_id.
+      res.status(200).json({ // Send 200 status upon success.
+        status: 'success',
+        data: data,
+        message: 'Here\'s that user you asked for'
+      });
+    })
+    .catch(function (err) {
+      res.sendStatus(400)
+      console.log('Unfortunately I was unable to find that user\'s information', err);
+      return next();
     });
-  })
-  .catch(err => res.send('ERROR: Couldnt grab that post from the DB', err))
 };
-
-const getPosts = function(req, res, next){
-  Post.findAll({})
-  .then((response) => {
-    res.status(200)
-    res.send(JSON.stringify({
-      status: 'success',
-      data: response.data,
-      message: 'Here are all the posts!'
-    }))
-    return next();
+//get all users
+const getUsers = function (req, res, next) {
+  User.findAll({})
+    .then((users) => {
+      res.status(200);
+      res.send(JSON.stringify({
+        status: 'success',
+        data: users,
+        message: 'Here are all the users!'
+      }));
+      return next();
+    })
+    .catch(err => {
+      res.status(400);
+      return next();
+    });
+};
+//Update User
+//! UPDATE
+const updateUser = function (req, res, next) {
+  User.update({
+    // username: newUsername,
+    // }, {
+    // where: {
+    //   id: id
+    // }
   })
-  .catch(err => {
-    console.log(err)
-    return next();
+    .then((newName) => {
+      res.status(201);
+      console.log(`This username has been updated to ${newName}`);
+    });
+  return next();
+};
+//! DELETE USER
+const deleteUser = function (req, res, next) {
+  User.destroy({
+    where: {
+      id: id,
+      username: username
+    }
   })
+    .then(() => {
+      res.status(201);
+      console.log('This user has been deleted');
+    });
 }
-
-  module.exports = {
-    getUsers,
-    getPosts,
-    getSinglePost,
-    createUser,
-
+//! POST CRUD
+//! CREATE POST
+const createPost = function (req, res) {
+  Post.create({
+    title: title,
+    postHoodId: postHoodId,
+    postTypeId: goal_picture,
+    postBody: postBody,
+    postVotes: 0
+  })
+    .then(() => {
+      Hood.create({})
+    })
+    .then(() => {
+      PostTypes.create({})
+    })
+    .then((data) => {
+      res.status(201)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Created a new Post!'
+        })
+    })
+    .catch((err) => {
+      res.status(400)
+      console.log('There was an error creating that post!'), err;
+      return next();
+    });
 }
+const getSinglePost = function (req, res) {
+  const id = req.params.postId;
+  Post.findOne({
+    where: {
+      title: title,
+      id: id
+    }
+  })
+    .then((singlePost) => {
+      res.status(200).json({
+        data: singlePost
+      });
+    })
+    .catch(err => res.sendStatus(400));
+};
+//get all the posts or comments from the db based on user id
+//!READ POST
+const getPosts = function (req, res, next) {
+  Post.findAll({
+    where: {
+      id: 1,
+    }
+  })
+    .then((response) => {
+      res.status(200);
+      res.send(JSON.stringify({
+        status: 'success',
+        data: response.data,
+        message: 'Here are all that user\'s posts!'
+      }));
+      return next();
+    })
+    .catch(err => {
+      res.sendStatus(400)
+      console.log(err);
+      return next();
+    });
+};
+//!UPDATE POST
+const updatePost = function (req, res, next) {
+  Post.update({
+    // title: newTitle,
+    // postBody: newPostBody,
+    // }, {
+    // where: {
+    //   id: postId
+    // }
+  })
+    .then((newPost) => {
+      res.status(201);
+      console.log(`This post has been updated to ${newPost}`);
+    });
+};
+//delete a specific post by iD
+//!DELETE POST
+const deletePost = function (req, res, next) {
+  Post.destroy({
+    where: {
+      id: id,
+    }
+  })
+    .then(() => {
+      res.status(201);
+      console.log('This post has been deleted');
+    });
+}
+//COMMENT CRUD
+module.exports = {
+  createUser,
+  getSingleUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+  createPost,
+  getSinglePost,
+  getPosts,
+  updatePost,
+  deletePost,
+};
--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,29 @@
-const ctrl = require('./db/Controllers');
-
-module.exports = function(app, express){
-  app.get('/', ctrl.getPosts)
-  app.post('/signup', ctrl.createUser)
+const ctrl = require('./db/controllers');
+/*
+signup
+login
+users/post:id
+user:id
+posts
+*/
+module.exports = function (app, express) {
+  //login to an account/get a single user
+  app.post('/signup', ctrl.createUser);
+  //get single user info
+  app.get('/users:id', ctrl.getSingleUser);
   //get all users
   app.get('/users', ctrl.getUsers);
-  //get single user post
+  //update a user
+  // ? app.put('/users:id/', ctrl.updateUser)
+  //create a post
+  app.post('/posts', ctrl.createPost);
+  // get all the posts
+  app.get('/posts', ctrl.getPosts);
+  //get single user's post
   app.get('/users:id/post', ctrl.getSinglePost);
- // app.post('/users:id', )
-}
+  // deleteUser,
+  // ? app.put('/posts:id'. ctrl.updatePost);
+  //delete a post
+  app.delete('users:id/posts:id', ctrl.deletePost);
+};
+
