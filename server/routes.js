34906fa38b184c4cc2c8eb2176002c8113a606d9
Ryan Moragas <ryanmoragas@gmail.com>
2020-01-02 12:21:58 -0600

    added weather endpoint

--- server/routes.js
+++ server/routes.js
@@ -25,4 +25,20 @@ module.exports = function (app, express) {
  // ? app.put('/posts:id'. ctrl.updatePost);
   //delete a post
   app.delete('users:id/posts:id', ctrl.deletePost);
+  //darksky current weather
+  app.get('/weather', (req, res) => {
+    axios.get(`https://api.darksky.net/forecast/${weatherKey}/29.9511, -90.0715`)
+      .then((darkSky) => {
+        darkSkyData = darkSky.data;
+        debugger;
+        res.status(200).json({ // Send 200 status upon success.
+          status: 'success',
+          data: darkSkyData,
+          message: 'NOLA weather from darkSky'
+        });
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
 };
