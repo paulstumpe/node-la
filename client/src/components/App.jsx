870eb83d352a784c4f91c7725bb7a10c8ac77446
Ryan Moragas <ryanmoragas@gmail.com>
2020-01-04 15:15:42 -0600

    added userPosts state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -17,6 +17,7 @@ class App extends React.Component {
       neighborhood: '',
       posts: [],
       currentPost: {},
+      userPosts: {},
       username: '',
       loggedIn: false,
       weather: {},
